1a3ef48091b5eafe0b8a33cd45553bb97d81b7ad
belus90 <belus90@gmail.com>
2022-03-19 20:57:41 +0000

    member_class: select_all and select by id

--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -10,3 +10,24 @@ def save(member):
     result = run_sql(sql, values)
     member.id = result[0]['id']
     return member
+
+def select_all():
+    members = []
+
+    sql = "SELECT * FROM members"
+    results = run_sql(sql)
+    for row in results:
+        member = Member(row['first_name'], row['last_name'],row['date_of_birth'], row['email_address'], row['id'])
+        members.append(member)
+    return members
+
+def select(id):
+    member = None
+    sql = "SELECT * FROM memberes WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        member = Member(result['first_name'], result['last_name'], result['date_of_birth'], result['email_address'], result['id'] )
+    return member
+
